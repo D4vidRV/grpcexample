310a381225be3da9adf3987db082333bafa3be7b
David <avalon@DESKTOP-MQVAOH6>
2023-04-20 13:41:27 -0600

    Improvement in the architecture

--- server/main.go
+++ server/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"io"
 	"log"
 	"net"
+	"time"
 
 	pb "github.com/D4vidRV/grpcexample/proto"
 
@@ -31,4 +34,67 @@ func main() {
 		log.Fatalf("Failed to start: %v", err.Error())
 	}
 
+}
+
+func (s *helloServer) SayHello(ctx context.Context, req *pb.NoParam) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{
+		Message: "Hello",
+	}, nil
+}
+
+func (s *helloServer) SayHelloServerStreaming(req *pb.NamesList, stream pb.GreetService_SayHelloServerStreamingServer) error {
+	log.Printf("Got request with names: %v", req.Names)
+
+	for _, name := range req.Names {
+		res := &pb.HelloResponse{
+			Message: "Hello " + name,
+		}
+
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+		time.Sleep(time.Second)
+	}
+	return nil
+}
+
+func (s *helloServer) SayHelloClientStreaming(stream pb.GreetService_SayHelloClientStreamingServer) error {
+	var messages []string
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.MessagesList{Messages: messages})
+		}
+
+		if err != nil {
+			return err
+		}
+		log.Printf("Got request with name: %v", req.Name)
+		messages = append(messages, "Hello "+req.Name)
+	}
+}
+
+func (s *helloServer) SayHelloBidirectionalStreaming(stream pb.GreetService_SayHelloBidirectionalStreamingServer) error {
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		log.Printf("Got request with name: %v", req.Name)
+
+		res := &pb.HelloResponse{
+			Message: "Hello " + req.Name,
+		}
+
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+	}
 }
